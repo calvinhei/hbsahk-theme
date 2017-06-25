e77ef868dd6400e103dd409c224306da9302cc52
calvinhei <calvin0302@icloud.com>
2017-06-25 17:20:20 +0800

    Update calendar.js

--- main-theme/js/calendar.js
+++ main-theme/js/calendar.js
@@ -23,6 +23,27 @@ var events_json = "events.json";
 var tiva_events = [];
 var order_num = 0;
 
+// Day, Month
+var wordMonth = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+var wordDay_sun = "Sunday";
+var wordDay_mon = "Monday";
+var wordDay_tue = "Tuesday";
+var wordDay_wed = "Wednesday";
+var wordDay_thu = "Thursday";
+var wordDay_fri = "Friday";
+var wordDay_sat = "Saturday";
+
+// View Button
+var calendar_view = "Calendar"
+var list_view = "List"
+var back = "Back"
+
+// Calendar Button
+var prev_year = "Prev Year"
+var prev_month = "Prev Month"
+var next_month = "Next Month"
+var next_year = "Next Year"
+
 // Config variable
 var wordDay;
 var date_start;
